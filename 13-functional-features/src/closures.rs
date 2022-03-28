982ce9ba05402e81905d0cb577799c2c78932887
Höhl, Lukas <lukas.hoehl@accso.de>
2022-03-28 16:16:19 +0200

    fix: generic cacher methodic

--- 13-functional-features/src/closures.rs
+++ 13-functional-features/src/closures.rs
@@ -1,4 +1,4 @@
-use std::{any::type_name, collections::HashMap, hash::Hash, thread, time::Duration};
+use std::{collections::HashMap, hash::Hash, thread, time::Duration};
 pub fn run() {
     let simulated_user_specified_value = 10;
     let simulated_random_number = 7;
@@ -9,61 +9,57 @@ fn generate_workout(intensitiy: u32, random_number: u32) {
     let mut expensive_result = Cacher::new(|num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
-        &num
+        num
     });
     if intensitiy < 25 {
-        println!("Today, do {} pushups", expensive_result.value(&intensitiy));
-        println!("Next, do {} situps", expensive_result.value(&intensitiy));
+        println!("Today, do {} pushups", expensive_result.value(intensitiy));
+        println!("Next, do {} situps", expensive_result.value(intensitiy));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!")
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result.value(&intensitiy)
+                expensive_result.value(intensitiy)
             );
         }
     }
 }
 
-struct Cacher<'a, T, K: 'a>
-where
-    T: Fn(&'a K) -> &'a K,
-{
+struct Cacher<T, K, V> {
     calculation: T,
-    cache: HashMap<&'a K, &'a K>,
+    cache: HashMap<K, V>,
 }
 
-impl<'a, T, K> Cacher<'a, T, K>
+impl<T, K, V> Cacher<T, K, V>
 where
-    T: Fn(&'a K) -> &'a K,
-    K: Hash + Eq,
+    T: Fn(K) -> V,
+    K: Hash + Eq + Clone,
+    V: Clone
 {
-    fn new(calculation: T) -> Cacher<'a, T, K> {
+    fn new(calculation: T) -> Cacher<T, K, V> {
         Cacher {
             calculation,
             cache: HashMap::new(),
         }
     }
-    fn value(&mut self, arg: &'a K) -> &'a K {
-        match self.cache.get(arg) {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.cache.insert(arg, v);
-                v
-            }
+    fn value(&mut self, arg: K) -> V {
+        if self.cache.contains_key(&arg) {
+            return self.cache.get(&arg).unwrap().clone();
         }
+        let v = (self.calculation)(arg.clone());
+        self.cache.insert(arg.clone(), v);
+        self.cache.get(&arg).unwrap().clone()
     }
 }
 
 #[test]
 fn call_with_different_values() {
-    let mut c = Cacher::new(|a| a);
+    let mut c = Cacher::new(|a: i32| a);
 
-    let v1 = c.value(&1);
-    let v2 = c.value(&2);
+    let v1 = c.value(1);
+    let v2 = c.value(2);
 
-    assert_eq!(*v1, 1);
-    assert_eq!(*v2, 2);
+    assert_eq!(v1, 1);
+    assert_eq!(v2, 2);
 }
